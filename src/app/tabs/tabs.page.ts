33376539aad888d1fec243622495202232aa42c2
ranp20 <polancoranp2019@gmail.com>
2020-11-14 14:21:05 -0500

    ner merge into branch 002

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,23 +9,4 @@ export class TabsPage implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-=======
-import { Component } from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
->>>>>>> 0af88d9... integration qr
-
-@Component({
-  selector: 'app-tabs',
-  templateUrl: './tabs.page.html',
-  styleUrls: ['./tabs.page.scss'],
-})
-export class TabsPage implements OnInit {
-  constructor() {}
-
-<<<<<<< HEAD
->>>>>>> ac861af... Initial commit
-=======
-  ngOnInit() {}
->>>>>>> 0af88d9... integration qr
 }
