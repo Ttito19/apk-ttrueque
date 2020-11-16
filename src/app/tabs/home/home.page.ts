29fc15985ffc93390e62d505dae9ef0362f6bc22
ranp20 <polancoranp2019@gmail.com>
2020-11-15 19:37:46 -0500

    new changes into david 029

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -5,7 +5,7 @@ import { HkApiproviderProvider } from "../../services/hk-apiprovider.service";
 import { ModalController } from "@ionic/angular";
 import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
 import { Base64ToGallery } from "@ionic-native/base64-to-gallery/ngx";
-import {Platform, ToastController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: "app-home",
   templateUrl: "./home.page.html",
@@ -57,8 +57,7 @@ export class HomePage  {
     public modalController: ModalController,
     private barcodeScanner:BarcodeScanner,
     private base64ToGallery:Base64ToGallery,
-    public toastController: ToastController,
-    private platform: Platform,
+    public toastController: ToastController
   
   ) {
     var data = JSON.parse(localStorage.getItem("user"));
