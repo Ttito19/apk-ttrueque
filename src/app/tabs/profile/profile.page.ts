21db08c5f4f65248066034c26181d4db636d941d
ranp20 <polancoranp2019@gmail.com>
2021-01-12 14:42:16 -0500

    new merge 005

---
+++ src/app/tabs/profile/profile.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+  public userDetails: any;
+  userData = {
+    nombre_cliente: "",
+    apellido_cliente: "",
+    condicion:"",
+    logo:"",
+    nameStore:"",
+    puntos:""
+  };
+
+  constructor() {
+    var data = JSON.parse(localStorage.getItem("user"));
+    if (data) {
+      this.userDetails = data.userData;
+      this.userData.nombre_cliente = this.userDetails.nombre_cliente;
+      this.userData.apellido_cliente = this.userDetails.apellido_cliente;
+      this.userData.logo = this.userDetails.logo;
+      this.userData.nameStore = this.userDetails.nombre_tienda;
+      this.userData.puntos = this.userDetails.puntos;
+    
+
+      this.userData.condicion = this.userDetails.condicion;
+    }
+    
+   }
+ 
+  ngOnInit() {
+  }
+
+}
