f99a8b8d2ca67942c65f41ded3a4c91bb5ce023e
ranp20 <polancoranp2019@gmail.com>
2021-01-12 14:45:42 -0500

    new merges 0001s2

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { LanguageService } from "../../services/language-service";
-
 import { HkApiproviderProvider } from "../../services/hk-apiprovider.service";
-import { ModalController } from "@ionic/angular";
+import { ModalController,Platform, ToastController,NavController } from "@ionic/angular";
 import { BarcodeScanner,BarcodeScannerOptions } from "@ionic-native/barcode-scanner/ngx";
+import { AlertController } from '@ionic/angular';
 // import { Base64ToGallery } from "@ionic-native/base64-to-gallery/ngx";
-import {Platform, ToastController } from '@ionic/angular';
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.page.html",
@@ -30,16 +30,20 @@ export class HomePage  {
     id:"",
     email:"",
     puntos:"",
+    points:"",
+    // puntoscliente:""
     
   };
-
- 
+  public datauser: any;
+  selectuser = {};
 
   qrData="" ;
   scannedCode=null;
   client=null;
 
   nameclient=null;
+  puntoscliente=null;
+
   elementType:"url"|"canvas"|"img"="canvas";
 
  
@@ -49,8 +53,11 @@ export class HomePage  {
     public modalController: ModalController,
     private barcodeScanner:BarcodeScanner,
     // private base64ToGallery:Base64ToGallery,
+    private _navController: NavController,
     public toastController: ToastController,
     private platform: Platform,
+    public alertController: AlertController,
+   
   
   ) {
     var data = JSON.parse(localStorage.getItem("user"));
@@ -60,8 +67,10 @@ export class HomePage  {
       this.userData.apellido_cliente = this.userDetails.apellido_cliente;
       this.userData.condicion = this.userDetails.condicion;
       this.userData.id = this.userDetails.id_cliente;
+
       this.userData.email = this.userDetails.nombre_cliente+" , "+this.userData.apellido_cliente; 
     }
+<<<<<<< HEAD
 <<<<<<< HEAD
     this.qrData="https://trueque.ga/home/"+ this.userData.id+"/"+this.userData.email;
 <<<<<<< HEAD
@@ -79,11 +88,18 @@ export class HomePage  {
 =======
     this.qrData= this.userData.id+"/"+this.userData.email;
 >>>>>>> 4c2b2d9... get name qr
+=======
+
+    
+>>>>>>> a2d0231... validate points
     this.postData.token = HkApiproviderProvider.gettoken();
     this.languageService.setInitiallanguage();
   
-
+this.getUserId()
   }
+
+ 
+
 
   scanCode(){
     const options: BarcodeScannerOptions = {
@@ -102,22 +118,48 @@ export class HomePage  {
       this.barcodeScanner.scan(options).then(
         barcodeData=>{
           this.scannedCode=barcodeData.text;
-          let get= this.scannedCode.split("/");
-
+          let get= this.scannedCode.split("/");   
+          this.puntoscliente=get[2];
           this.nameclient=get[1];
           this.client=get[0];
+
 
         }
       );
     }
+  }
 
-  
+  async presentAlert(msg) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Mensaje',    
+      message: msg,
+      buttons: ['OK']
+    });
 
-
+    await alert.present();
   }
 
 
   confirm() {
+
+
+  if( this.userData.puntos==""){
+  let msg="Escribir monto a cobrar";
+    this.presentAlert(msg);
+  
+  }else{
+
+
+    if(parseInt(this.puntoscliente)<parseInt(this.userData.puntos)){
+      let msg="El cliente no tiene puntos suficientes...";
+      this.presentAlert(msg);
+    }  else if(parseInt(this.puntoscliente)==0){
+
+      let msg="El cliente no tiene puntos...";
+      this.presentAlert(msg);
+    } else{
+      this._navController.navigateRoot("/lateral/confirm");
 	
     this.userDataConfirm = {
      
@@ -130,7 +172,7 @@ export class HomePage  {
 		this.auth.postData(this.userDataConfirm, 'restpoints').then(
 			(result) => { 
         this.resposeData = result;
-       alert("Pago con éxito")
+        this._navController.navigateRoot("/lateral/confirm");
 			
 			},
 			(err) => {
@@ -138,6 +180,47 @@ export class HomePage  {
         
 			}
 		);
+    }
+
+  
+  }
+
+   
 	}
+
+  getUserId(){
+   
+//  console.log(this.selectuser)
+    this.selectuser={
+      id_cliente: this.userDetails.id_cliente,
+      token:this.userDetails.token
+    }
+      this.auth.postData(this.selectuser, "selectuserid").then(
+        (result) => {
+          this.resposeData = result;
+          this.datauser= this.resposeData.userData;
+          this.userData.points=this.datauser.puntos;
+          this.qrData= this.userData.id+"/"+this.userData.email+"/"+this.userData.points;
+        },
+        (err) => {
+        console.log(err);
+        
+        
+        }
+      );
+    }
+    
+
+
+    doRefresh(event) {
+      
+      this.getUserId();
+      setTimeout(() => {
+        console.log('Async operation has ended');
+        event.target.complete();
+      }, 1000);
+    }
+  
+  
 
 }
